b0747dca72c3109d502cc964a40f20e42964c704
8884khan <80153118+8884khan@users.noreply.github.com>
2022-05-15 06:25:54 -0700

    Update pattern 2.py

--- pattern 2.py
+++ pattern 2.py
@@ -27,3 +27,21 @@ for i in range(1,n+1):
     for j in range(1,n+1):
         print(chr(96+j),end=" ")
     print()
+
+    n = int(input("Enter the number"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+print("-------------------------")
+n = int(input("Enter the number"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+print("=================================")
+n = int(input("Enter the number"))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(65+n-i),end=" ")
+    print() 
